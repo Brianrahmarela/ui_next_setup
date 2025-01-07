fcd8070eed82128732f8c982a9df4c125fc61153
Brian <brian.rahmarela@gmail.com>
2025-01-07 10:42:35 +0700

    activated bfcache browser except login route in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
   if (req.nextUrl.pathname.startsWith('/login') && !token) {
     return;
   }
-  
+
   if (req.nextUrl.pathname === '/' && !token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
@@ -14,18 +14,25 @@ export async function middleware(req) {
   if (req.nextUrl.pathname.startsWith('/home') && !token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
-  
+
   if (req.nextUrl.pathname === '/' && token) {
     return NextResponse.redirect(new URL('/home', req.url));
   }
-  
+
   if (req.nextUrl.pathname.startsWith('/login') && token) {
     return NextResponse.redirect(new URL('/home', req.url));
   }
 
-  // Menambahkan header Cache-Control untuk tidak menyimpan cache
+  // Menambahkan header Cache-Control
   const response = NextResponse.next();
-  response.headers.set('Cache-Control', 'no-store');
+
+  // Tidak menambahkan no-store di halaman yang ingin di-cache
+  if (req.nextUrl.pathname.startsWith('/login')) {
+    response.headers.set('Cache-Control', 'no-store');  // Halaman login tidak boleh di-cache
+  } else {
+    response.headers.set('Cache-Control', 'public, max-age=0, must-revalidate');  // Halaman lainnya boleh di-cache
+  }
+
   return response;
 }
 
